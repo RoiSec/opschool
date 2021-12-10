ab9dee2f7dfc8b930845b6f03c0f7a322320e3cf
RoiSec <roi>
2021-12-10 16:39:03 +0200

    add answer for excersie 6

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+#This script using scp to transfer files to remote server2.with awk i filted the size and print the sum of them.
+script -q -c "scp $1 $2  vagrant@server2:$3"
+size=$(ls -l $1 | awk '{print $5}')
+size="$(($size + $(ls -l $2 | awk '{print $5}')))"
+echo $size
